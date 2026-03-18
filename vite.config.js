6e2ac931d2a97f1de98a8612dd25f7307ad7e2ff
Raja Sai Datha Sushanth <rajasaidathasushanth2006@gmail.com>
2026-03-18 21:41:07 +0530

    l

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://inventory-management-and-billing-system-12oh.onrender.com',
         changeOrigin: true,
       },
     },
